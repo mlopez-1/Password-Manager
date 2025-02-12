a71a964522fabce8949f92613291626df19c32a3
Michaell <michael.a.lopez574@gmail.com>
2025-02-12 13:34:10 -0500

    Created basic framework for how PasswordManager should handle reading files using FileHandler

--- src/PasswordManager.java
+++ src/PasswordManager.java
@@ -5,22 +5,26 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class PasswordManager {
-    private HashMap<String, Password> passwords;
+    private Map<String, String> passwordMap;
     private FileHandler fileHandler;
 
     public PasswordManager() {
-
+        passwordMap = new HashMap<>();
+        fileHandler = new FileHandler();
+        loadPasswordsFromFile(); //Currently does nothing
     }
 
     public void addPassword(String userName, String plaintTextPassword) throws NoSuchPaddingException, IllegalBlockSizeException, InvalidKeySpecException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
-        Password password = new Password(userName, EncryptionUtil.encryptPassword(plaintTextPassword));
-
+        Password newPassword = new Password(userName, EncryptionUtil.encryptPassword(plaintTextPassword));
+        passwordMap.put(newPassword.userName, newPassword.password);
         //For debugging
-        System.out.println(password.toString());
+        System.out.println(newPassword.toString());
     }
 
+    //Cant implement for now
     public String retrievePassword(String userName, String inputPassword) {
         return null;
     }
@@ -29,7 +33,8 @@ public class PasswordManager {
 
     }
 
-    private void loadPasswords() {
-
+    private void loadPasswordsFromFile() {
+        //Need to create loadPasswords method
+        //passwordMap = fileHandler.loadPasswords();
     }
 }
